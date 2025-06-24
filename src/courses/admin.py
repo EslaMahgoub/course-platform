d004fc8e9eb9ab34d3ac2479fb6939f21053529c
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2025-06-24 18:10:36 +0200

    Add Lesson model
    remove trailing spaces

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -10,14 +10,14 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['status', 'access']
     fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
     readonly_fields = ['display_image']
-        
+
     def display_image(self, obj, *args, **kwargs):
         url = obj.image_admin_url #property
         return format_html(f"<img src={url} />")
 
     display_image.short_description = "Current Image"
 
-    
+
 
 
 # admin.site.register(Course, CourseAdmin)
--- src/courses/models.py
+++ src/courses/models.py
@@ -12,7 +12,7 @@ class PublishStatus(models.TextChoices):
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
     EMAIL_REQUIRED = "email", "Email required"
-    
+
 def handle_upload(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     # return "user_{0}/{1}".format(instance.user.id, filename)
@@ -24,12 +24,12 @@ class Course(models.Model):
     # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
     image = CloudinaryField('image', null=True)
     access = models.CharField(
-        max_length=5, 
-        choices=AccessRequirement.choices, 
+        max_length=5,
+        choices=AccessRequirement.choices,
         default=AccessRequirement.EMAIL_REQUIRED)
     status = models.CharField(
-        max_length=10, 
-        choices=PublishStatus.choices, 
+        max_length=10,
+        choices=PublishStatus.choices,
         default=PublishStatus.DRAFT)
 
     @property
@@ -46,7 +46,7 @@ class Course(models.Model):
         }
         url = self.image.build_url(**image_options)
         return url
-    
+
     def get_image_thumbnial(self, as_html=False, width=500):
         if not self.image:
             return ""
@@ -59,6 +59,33 @@ class Course(models.Model):
         # CloudinaryImage(str(obj.image)).build_url(**image_options)
         url = self.image.build_url(**image_options)
         return url
+
+# Lesson.objects.all() # lesson queryset -> all rows
+# Lesson.objects.first()
+# course_obj = Course.objects.first()
+# course_qs = Course.objects.filter(id=course_obj.id)
+# Lesson.objects.filter(course__id=course_obj.id)
+# course_obj.lesson_set.all()
+# lesson_obj = Lesson.objects.first()
+# ne_course_obj = lesson_obj.course
+# ne_course_lessons = ne_course_obj.lesson_set.all()
+# lesson_obj.course_id
+# course_obj.lesson_set.all().order_by("-title")
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField("image", blank=True, null=True)
+    video = CloudinaryField("video", blank=True, null=True, resource_type="video")
+    can_preview = models.BooleanField(
+        default=False,
+        help_text="If user does not have access to course, can they see this?")
+    status = models.CharField(
+        max_length=10,
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED)
 
 """
 - Lessons:
