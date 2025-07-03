c5a5e37dd23b2f3d727f835f3dbf7ac1390681b2
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2025-07-03 17:16:46 +0200

    Add email services for verify and verification process

--- src/core/views.py
+++ src/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from emails.models import Email
+from emails.models import Email, EmailVerificationEvent
 from emails.forms import EmailForm
+from emails import services as email_services
 
 EMAIL_ADDRESS = settings.EMAIL_ADDRESS
+
 def home_view(request, *args, **kwargs):
     print(request.POST.get('email'))
     form = EmailForm(request.POST or None)
@@ -13,9 +15,8 @@ def home_view(request, *args, **kwargs):
         'message': ''
         }
     if form.is_valid():
-        email_value = form.cleaned_data.get('email')
-        obj = form.save()
-        email_obj, created = Email.objects.get_or_create(email=email_value)
+        email_val = form.cleaned_data.get('email')
+        obj = email_services.start_verification_event(email_val)
         print(obj)
         context['form'] = EmailForm()
         context['message'] = f'Success! Check your email for verification from {EMAIL_ADDRESS}'
