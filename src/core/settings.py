7cb56cd08f6cae9fd08d081c9f51c8214bfbfe34
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2025-07-03 22:01:34 +0200

    Setup and Apply Htmx on login form

--- src/core/settings.py
+++ src/core/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'django_htmx',
+
+    # Internal
     'courses',
     'emails',
 ]
@@ -52,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
--- src/emails/urls.py
+++ src/emails/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('hx/login/', views.email_token_login_view, name="login-email"),
     path('verify/<uuid:token>/', views.verify_email_token_view, name="verify-email"),
 ]
