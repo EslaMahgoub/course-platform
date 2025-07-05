638d55d17063f655f47a9417055c19190632a8bb
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2025-07-05 02:21:28 +0200

    Enhance lesson page layout and styling; update home page structure; add theme variables for fonts and colors
    - Improved the layout of the lesson page by adding a structured section with video embedding, title, ratings, and action buttons.
    - Included a description area and styled components for better user experience.
    - Updated the home page to include header sections and dynamic content loading.
    - Introduced theme variables in the CSS for consistent font and color usage across the application.

--- src/courses/views.py
+++ src/courses/views.py
@@ -6,18 +6,22 @@ from . import services
 
 def course_list_view(request):
     queryset = services.get_publish_courses()
-    context = {"object_list": queryset}
-    return render(request, "courses/list.html", context)
+    context = {'object_list': queryset}
+    template_name = 'courses/list.html'
+    if request.htmx:
+        template_name = 'courses\snippets\list-display.html'
+        context['queryset'] = queryset[:3]
+    return render(request, template_name, context)
 
 def course_detail_view(request, course_id=None, *args, **kwargs):
     course_obj = services.get_course_detail(course_id=course_id)
     if course_obj is None:
         raise Http404
     lessons_queryset = services.get_course_lessons(course_obj)
-    context = {"object": course_obj,
-            "lessons_queryset": lessons_queryset
+    context = {'object': course_obj,
+            'lessons_queryset': lessons_queryset
             }
-    return render(request, "courses/detail.html", context)
+    return render(request, 'courses/detail.html', context)
 
 def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs):
     lesson_obj = services.get_lesson_detail(lesson_id=lesson_id, course_id=course_id)
@@ -27,23 +31,23 @@ def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs)
     print(request.path)
     if lesson_obj.requires_email and not email_id_exists:
         request.session['next_url'] = request.path
-        return render(request, "courses/email-required.html", {})
-    # template_name = "courses/purchase-required.html"
-    template_name = "courses/lesson-coming-soon.html"
+        return render(request, 'courses/email-required.html', {})
+    # template_name = 'courses/purchase-required.html'
+    template_name = 'courses/lesson-coming-soon.html'
     context = {
-        "object": lesson_obj
+        'object': lesson_obj
     }
     if not lesson_obj.is_coming_soon and lesson_obj.has_video:
         """
         Lesson is published, go forward
         Video is available
         """
-        template_name = "courses/lesson.html"
+        template_name = 'courses/lesson.html'
         video_embed_html = helpers.get_cloudinary_video_object(
             lesson_obj,
-            field_name="video",
+            field_name='video',
             as_html=True,
-            width=1250
+            width=550
             )
         context['video_embed'] = video_embed_html
     return render(request, template_name, context)
